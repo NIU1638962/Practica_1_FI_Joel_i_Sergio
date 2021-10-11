44b7cedc9e27f3729798cb467f72c88c4cd93dc1
Joel Tapia Salvador <Joel.TapiaS@autonoma.cat>
2021-10-11 10:18:53 +0200

    A partir de la comanda 1 no funcionen del tot bé, copia de seguretat per fer canvis i no perdre progres

--- Practica1.sh
+++ Practica1.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-cut -f1-11,13-15 CAvideos.csv>CAvideos1.csv
+cut -d, -f1-11,13-15 CAvideos.csv>CAvideos1.csv
 awk '$12!=True && $12!=False' CAvideos1.csv>CAvideos2.csv
-awk '$14!=True' CAvideos2.csv>CAvideos3.csv
+awk '$13!=True && $13!=False' CAvideos2.csv>CAvideos3.csv
+awk '$14!=True && $14!=False' CAvideos3.csv>CAvideos4.csv
+awk '$14!=True' CAvideos4.csv>CAvideos5.csv
