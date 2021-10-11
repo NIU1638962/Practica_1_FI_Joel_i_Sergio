913a64245d5fb7ebd426973e657a328e83044c0c
Joel Tapia Salvador <Joel.TapiaS@autonoma.cat>
2021-10-11 10:58:01 +0200

    L'arxiu ja fa el pas 1, 2 i 3 i manté la primera linea amb els nombs de les columnes

--- Practica1.sh
+++ Practica1.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cut -d, -f1-11,13-15 CAvideos.csv>CAvideos1.csv
-awk -F',' '$12=="True" || 12=="False" && $14!="True"' CAvideos1.csv>CAvideos2.csv
+awk -F',' 'NR==1 || $12=="True" || 12=="False" && $14!="True"' CAvideos1.csv>CAvideos2.csv
