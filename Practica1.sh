30b97991047c82fc7efa8937b9c5c12067c727c9
Joel Tapia Salvador <Joel.TapiaS@autonoma.cat>
2021-10-11 09:47:36 +0200

    Segona i tercera linea correctes

--- Practica1.sh
+++ Practica1.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-cut -d, -f1-11,13-15 CAvideos.csv>CAvideos1.csv
-awk -F',' ' { print S1,S2 }' CAvideos.csv | wc -l
+cut -f1-11,13-15 CAvideos.csv>CAvideos1.csv
+awk '$12!=True && $12!=False' CAvideos1.csv>CAvideos2.csv
+awk '$14!=True' CAvideos2.csv>CAvideos3.csv
